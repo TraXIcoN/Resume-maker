09d46ef630212e636803f231b2d91e67af876dd3
Aditya Mohan <coding.traxicon16@gmail.com>
2023-11-20 23:01:03 -0630

    Added API_key

--- gpt_summary.py
+++ gpt_summary.py
@@ -1,6 +1,6 @@
 import openai
 
-openai.api_key = "YOUR OPENAI KEY"
+openai.api_key = "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"
 
 def get_summary_for_resume(description):
     response = openai.Completion.create(
