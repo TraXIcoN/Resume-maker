42e37525f7de310cb491c7785d6f7a8a77f25fc0
Aditya Mohan <coding.traxicon16@gmail.com>
2023-11-21 00:01:03 -0630

    removed apikey

--- gpt_summary.py
+++ gpt_summary.py
@@ -1,19 +1,18 @@
-import openai
+from openai import OpenAI
 
-openai.api_key = "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"
+client = OpenAI(api_key="API_KEY")
+
 
 def get_summary_for_resume(description):
-    response = openai.Completion.create(
-        model="text-davinci-003",
-        prompt="Summarize the following in bullet points for my resume \n\n"+description,
-        temperature=0,
-        max_tokens=256,
-        top_p=1,
-        frequency_penalty=0,
-        presence_penalty=0
-    )
-    if len(response['choices'][0]) > 0:
-        points = response['choices'][0]['text'].split("\n")
+    response = client.completions.create(model="gpt-3.5-turbo-instruct",
+    prompt="Summarize the following in bullet points for my resume \n\n"+description,
+    temperature=0,
+    max_tokens=256,
+    top_p=1,
+    frequency_penalty=0,
+    presence_penalty=0)
+    if len(response.choices[0]) > 0:
+        points = response.choices[0].text.split("\n")
         s = ""
         # Remove the Bullet point from the response text
         for point in points:
@@ -22,17 +21,15 @@ def get_summary_for_resume(description):
     return ""
 
 def get_summary_for_projects(description):
-    response = openai.Completion.create(
-        model="text-davinci-003",
-        prompt="Summarize the following in 2 bullet points for my resume \n\n"+description,
-        temperature=0,
-        max_tokens=256,
-        top_p=1,
-        frequency_penalty=0,
-        presence_penalty=0
-    )
-    if len(response['choices'][0]) > 0:
-        points = response['choices'][0]['text'].split("\n")
+    response = client.completions.create(model="gpt-3.5-turbo-instruct",
+    prompt="Summarize the following in 2 bullet points for my resume \n\n"+description,
+    temperature=0,
+    max_tokens=256,
+    top_p=1,
+    frequency_penalty=0,
+    presence_penalty=0)
+    if len(response.choices[0]) > 0:
+        points = response.choices[0].text.split("\n")
         s = ""
         # Remove the Bullet point from the response text
         for point in points:
